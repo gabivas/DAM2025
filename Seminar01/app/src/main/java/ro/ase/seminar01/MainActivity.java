13b8aad7cb2f7dcd20cc798db44d678fb4e017a4
gabriel.vasilescu <gabriel.vasilescu@eos-it.ro>
2025-10-09 20:39:31 +0300

    Seminar 01

---
+++ Seminar01/app/src/main/java/ro/ase/seminar01/MainActivity.java
@@ -0,0 +1,33 @@
+package ro.ase.seminar01;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("tagOnResume", "Am ajuns in starea onResume()");
+        Toast.makeText(this, "Am ajuns in starea onResume()", Toast.LENGTH_LONG).show();
+    }
+}
